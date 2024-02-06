cbe19d915f86e19a8cb8f957b63e9382871d9fff
camilo <camiloiglesias82@gmail.com>
2024-02-06 19:48:59 -0300

    const estaCorreta

--- index.js
+++ index.js
@@ -108,6 +108,7 @@ const perguntas = [
 const quiz = document.querySelector('#quiz')
 const template = document.querySelector('template')
 
+const corretas = new Set()
 
 //loop ou laço de repetição
 
@@ -119,6 +120,18 @@ for(const item of perguntas) {
  for(let resposta of item.respostas) {
     const dt = quizItem.querySelector('dl dt').cloneNode(true)
     dt.querySelector('span').textContent = resposta
+    dt.querySelector('input').setAttribute('name', 'pergunta-' + perguntas.indexOf(item))
+    dt.querySelector('input').value = item.respostas.indexOf(resposta)
+
+    dt.querySelector('input').onchange = (event) => {
+     const estaCorreta = event.target.value == item.correta
+     
+     corretas.delete(item)
+     if(estaCorreta){
+      corretas.add(item)
+     }
+     alert(corretas.size)
+    }
 
     quizItem.querySelector('dl').appendChild(dt)
  }
